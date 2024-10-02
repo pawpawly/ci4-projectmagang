69fd810bc37efa7101de13b163dbc9c7311eaa81
Ganang <rbfamilysmd@gmail.com>
2024-10-02 09:33:53 +0800

    footerv2

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,74 +1,52 @@
-<footer class="bg-gray-100 text-gray-700 py-8">
+<footer class="bg-white text-gray-700 py-8">
     <div class="container mx-auto px-6">
         <div class="flex flex-wrap justify-between">
-            <!-- Left side: Logo and Social media -->
-            <div class="w-full md:w-1/4 mb-6">
-                <img src="pict/icon.png" alt="Logo" class="mb-4 w-24 h-auto">
-                <p class="mb-4">Dinas Kebudayaan, Kepemudaan, Olahraga, Dan Parawisata Kota Banjarmasin </p>
-                <div class="flex space-x-4">
-                            <!-- Instagram Icon -->
-                    <a href="https://www.instagram.com/budporaparbjm/?hl=id" target="_blank" class="hover:text-gray-600">
-                        <img src="pict/instagramlogo.png" alt="Instagram" class="w-8 h-8">
-                    </a>
-                            <!-- Facebook Icon -->
-                    <a href="https://web.facebook.com/budporaparbjm/?_rdc=1&_rdr" target="_blank" class="hover:text-gray-600">
-                        <img src="pict/facebooklogo.png" alt="Facebook" class="w-8 h-8">
-                    </a>
-                            <!-- Twitter Icon -->
-                            <a href="https://twitter.com/budparbjm" target="_blank" class="hover:text-gray-600">
-                        <img src="pict/xlogo.png" alt="x" class="w-8 h-8">
-                    </a>
-                            <!-- Youtube Icon -->
-                    <a href="https://www.youtube.com/channel/UCoCwWPlc0d4Xg-xSxKly3Aw" target="_blank" class="hover:text-gray-600">
-                        <img src="pict/youtubelogo.png" alt="x" class="w-11 h-8">
-                    </a>
-                </div>
+            <!-- Left Side: Visit Us -->
+            <div class="w-full md:w-1/3 mb-6">
+                <h3 class="text-gray-800 text-lg font-semibold mb-4">Visit Us</h3>
+                <p>Jl. Teluk Kelayan No.17, RT.005/RW.001,<br>
+                Kelayan Bar., Kec. Banjarmasin Sel., Kota Banjarmasin,<br>
+                Kalimantan Selatan 70234<br>
+                WhatsApp: 08xx-xxxx-xxxx</p>
             </div>
 
-            <!-- Right side: Links Section -->
-            <div class="w-full md:w-3/4 flex justify-around">
-                <div>
-                    <h3 class="text-gray-600 font-semibold mb-3">Solutions</h3>
-                    <ul class="space-y-2">
-                        <li><a href="404page.php" class="hover:text-blue-500">Marketing</a></li>
-                        <li><a href="404page.php" class="hover:text-green-500">Analytics</a></li>
-                        <li><a href="404page.php" class="hover:text-purple-500">Commerce</a></li>
-                        <li><a href="404page.php" class="hover:text-orange-500">Insights</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3 class="text-gray-600 font-semibold mb-3">Support</h3>
-                    <ul class="space-y-2">
-                        <li><a href="pricing.php" class="hover:text-red-500">Pricing</a></li>
-                        <li><a href="404page.php" class="hover:text-yellow-500">Documentation</a></li>
-                        <li><a href="404page.php" class="hover:text-purple-500">Guides</a></li>
-                        <li><a href="404page.php" class="hover:text-pink-500">API Status</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3 class="text-gray-600 font-semibold mb-3">Company</h3>
-                    <ul class="space-y-2">
-                        <li><a href="about.php" class="hover:text-red-600">About</a></li>
-                        <li><a href="blog.php" class="hover:text-yellow-600">Blog</a></li>
-                        <li><a href="404page.php" class="hover:text-blue-600">Jobs</a></li>
-                        <li><a href="404page.php" class="hover:text-green-600">Press</a></li>
-                        <li><a href="404page.php" class="hover:text-purple-600">Partners</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3 class="text-gray-600 font-semibold mb-3">Legal</h3>
-                    <ul class="space-y-2">
-                        <li><a href="404page.php" class="hover:text-blue-600">Claim</a></li>
-                        <li><a href="404page.php" class="hover:text-pink-600">Privacy</a></li>
-                        <li><a href="404page.php" class="hover:text-yellow-600">Terms</a></li>
-                    </ul>
+            <!-- Middle Side: Google Maps -->
+            <div class="w-full md:w-1/3 mb-6 md:mr-8">
+                <iframe 
+                    src="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.0859696107373!2d114.5955349!3d-3.3289366000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de4230001a31ee7%3A0x8b3e5e488c0df3e2!2sMuseum%20Kayuh%20Baimbai!5e0!3m2!1sid!2sid!4v1727831963840!5m2!1sid!2sid"
+                    width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+
+            <!-- Right Side: Social Media -->
+            <div class="w-full md:w-1/4 mb-6 md:ml-auto text-right"> <!-- Geser media sosial ke kanan -->
+                <!-- Tambahkan Logo Museum -->
+                <!-- <img src="pict/logodisbudporapar.png" class="w-22 h-auto mx-auto md:mx-0 mb-4"> -->
+                
+                <h3 class="text-gray-800 text-lg font-semibold mb-4">Social Media</h3>
+                <div class="flex justify-end space-x-4"> <!-- Tambahkan justify-end untuk ikon sosial media -->
+                    <!-- Instagram Icon -->
+                    <a href="https://www.instagram.com" class="social-icon" target="_self">
+                        <img src="pict/instagramlogo.png" alt="Instagram" class="w-8 h-8 ">
+                    </a>
+                    <!-- Facebook Icon -->
+                    <a href="https://www.facebook.com" class="social-icon" target="_self">
+                        <img src="pict/facebooklogo.png" alt="Facebook" class="w-8 h-8 ">
+                    </a>
+                    <!-- Twitter Icon -->
+                    <a href="https://www.twitter.com" class="social-icon" target="_self">
+                        <img src="pict/xlogo.png" alt="Twitter" class="w-8 h-8 ">
+                    </a>
+                    <!-- YouTube Icon -->
+                    <a href="https://www.youtube.com" class="social-icon" target="_self">
+                        <img src="pict/youtubelogo.png" alt="YouTube" class="w-8 h-8 ">
+                    </a>
                 </div>
             </div>
         </div>
 
-        <!-- Footer bottom -->
-        <div class="mt-8 text-center text-gray-500 border-t border-gray-300 pt-4">
-            &copy; 2024 Cookies Tech, Inc. All rights reserved.
+        <!-- Footer Bottom -->
+        <div class="mt-8 text-center font-semibold text-gray-500 border-t border-gray-300 pt-4">
+            &copy; Museum Kayuh Baimbai 2024
         </div>
     </div>
 </footer>
--- app/Views/header.php
+++ app/Views/header.php
@@ -3,14 +3,13 @@
     <div class="logo">
       <a href="#"><img src="<?= base_url('pict/iconmuseum.png'); ?>" alt="Logo" class="h-10"></a>
     </div>
-    <nav class="space-x-4 flex items-center">
+    <nav class="space-x-4 flex items-center justify-center w-full"> <!-- Ubah justify-between menjadi justify-center -->
       <a href="#beranda" class="text-white hover:text-gray-300 font-semibold">Beranda</a>
       <a href="#jadwal" class="text-white hover:text-gray-300 font-semibold">Jadwal</a>
       <a href="#koleksi" class="text-white hover:text-gray-300 font-semibold">Koleksi</a>
       <a href="#event" class="text-white hover:text-gray-300 font-semibold">Event</a>
-      <a href="#tentangkami" class="text-white hover:text-gray-300 font-semibold">Tentang Kami</a>
-      <a href="<?= site_url('login');?>" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-400">Login</a>
-      <!-- <a href="#" class="bg-gradient-to-r from-yellow-400 to-red-500 text-white py-2 px-4 rounded hover:from-yellow-300 hover:to-red-400">Reservasi Kunjungan</a> -->
+      <a href="<?= site_url('aboutus');?>" class="text-white hover:text-gray-300 font-semibold">Tentang Kami</a>
     </nav>
+    <a href="<?= site_url('login');?>" class="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-400">Login</a> <!-- Pindahkan elemen login ke luar nav untuk tetap di kanan -->
   </div>
 </header>
