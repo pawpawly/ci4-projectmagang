f1328b72046c8b3d02be60414e2ba6969b914964
Ganang <rbfamilysmd@gmail.com>
2024-10-28 14:18:45 +0800

    adding forum in view home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,24 +2,28 @@
 
 namespace App\Controllers;
 use App\Models\EventModel; // Panggil model event
+use App\Models\BeritaModel; // Panggil model berita
 
 class Home extends BaseController
 {
     protected $eventModel;
+    protected $beritaModel;
 
     public function __construct()
     {
-        $this->eventModel = new EventModel(); // Inisialisasi model
+        $this->eventModel = new EventModel(); // Inisialisasi model event
+        $this->beritaModel = new BeritaModel(); // Inisialisasi model berita
     }
 
     public function index()
     {
-        $events = $this->eventModel->findAll(); // Pastikan tidak ada limit di sini
-    
+        $events = $this->eventModel->findAll(); // Ambil semua event
+        $berita = $this->beritaModel->getBeritaWithUser(); // Ambil semua berita dengan user
+
         return view('home', [
             'title' => 'Home',
             'events' => $events,
+            'berita' => $berita, // Kirim data berita ke view
         ]);
     }
-    
 }
